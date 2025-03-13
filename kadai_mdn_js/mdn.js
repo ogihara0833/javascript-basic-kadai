cc6c9da564f6d0e82082b1b924fa48a8e29ffe0b
ogihara0833 <140858355+ogihara0833@users.noreply.github.com>
2025-03-13 09:00:54 +0900

    kadai_mdn_jsのアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+ let today = new Date();
+ 
+ let year = today.getFullYear();
+ let month = today.getMonth() + 1;
+ let date = today.getDate();
+
+ console.log(year + '年' + month + '月' + date + '日');
